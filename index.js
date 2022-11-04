0e1a10185579691a1e39c2d3d66c22f327f28b8b
andriGo <afomin129@gmail.com>
2022-11-05 00:27:15 +0200

    one-line

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function byModule(props) {
-	const moneyForAllTheSalt = props.saltBeefBought * props.saltBeefPrice;
-	const moneyForAllTheMoneyRice = props.redRiceBought * props.redRicePrice;
-	const moneyForAllTheWine = props.oldVineBought * props.oldVinePrice;
-	const totalMoneySpent = moneyForAllTheMoneyRice + moneyForAllTheSalt + moneyForAllTheWine;
-	const theRestOfTheMoney = props.salary - totalMoneySpent;
-	const result = theRestOfTheMoney % props.exchange;
-	return result;
+	return (props.salary - ((props.saltBeefBought * props.saltBeefPrice) + (props.redRiceBought * props.redRicePrice) + (props.oldVineBought * props.oldVinePrice))) % props.exchange;
 }
 
 module.exports = byModule;
