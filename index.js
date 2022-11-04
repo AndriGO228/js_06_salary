cdd00fa71d4295958a15a0846efdd167fb4adfa1
andriGo <afomin129@gmail.com>
2022-11-05 00:16:05 +0200

    variables

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function byModule(props) {
-	// write your code here
+	const moneyForAllTheSalt = props.saltBeefBought * props.saltBeefPrice;
+	const moneyForAllTheMoneyRice = props.redRiceBought * props.redRicePrice;
+	const moneyForAllTheWine = props.oldVineBought * props.oldVinePrice;
+	const totalMoneySpent = moneyForAllTheMoneyRice + moneyForAllTheSalt + moneyForAllTheWine;
+	const theRestOfTheMoney = props.salary - totalMoneySpent;
+	const result = theRestOfTheMoney % props.exchange;
+	return result;
 }
 
 module.exports = byModule;
